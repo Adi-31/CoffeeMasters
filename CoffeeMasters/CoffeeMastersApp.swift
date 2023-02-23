3798b6902ef861c586ee3ae756128c16c8f50bcb
Adi-31 <mail4adithya31@gmail.com>
2023-02-23 11:39:13 +0530

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Adithya Praveen on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
